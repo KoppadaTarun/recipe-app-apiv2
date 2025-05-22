13bf33a3e451f82d7d06cc7689d355ad99c070ca
TarunEnma <tarun.koppada2000@gmail.com>
2025-05-22 08:38:36 +0530

    configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+calculatro functions
+
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x form y and return result."""
+
+    return y - x
